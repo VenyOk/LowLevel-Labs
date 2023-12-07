7cdb10e7bfb6de13d93eac550b77e0dc4525f286
Veniamin Shemyakin <122688291+VenyOk@users.noreply.github.com>
2023-12-07 14:39:58 +0300

    Create macro_2.asm

---
+++ lab5/variant8/macro_2.asm
@@ -0,0 +1,70 @@
+print macro string
+
+    xor ax, ax
+	mov dx, offset string
+	mov ah, 09h
+	int 21h
+
+endm
+
+input macro string
+
+	xor ax, ax
+	mov ah, 0ah
+	lea dx, string
+	int 21h
+
+endm
+
+solve macro string, result
+
+    mov si, offset string
+    inc si
+    inc si
+    mov di, offset result
+
+    search_first_symbol:
+        mov al, [si]
+        cmp al, '$'
+            je continue_macro
+        cmp al, ' '
+            jne search_spaces
+        inc si
+        jmp search_first_symbol
+        
+    search_spaces:
+
+        mov al, [si]
+        cmp al, '$'
+            je continue_macro
+        cmp al, ' '
+            je insert_space
+            jne insert_symb
+        inc si
+        jmp search_spaces
+
+    insert_space:
+        mov al, [si - 1]
+
+        cmp al, ' '
+            jne need_to_insert_space
+            je continue
+
+        need_to_insert_space:
+            mov bl, ' '
+            mov [di], bl
+            inc di
+        continue:
+            inc si
+            jmp search_spaces
+
+    insert_symb:
+
+        mov [di], al
+        inc di
+        inc si
+        jmp search_spaces
+
+    continue_macro:
+        xor ax, ax
+endm
